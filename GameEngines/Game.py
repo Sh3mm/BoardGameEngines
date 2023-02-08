3843ca9c2775d36358ad9eaa728d50d303626242
sh3mm <sh3mm@pop-os.localdomain>
2023-02-07 19:28:32 -0500

    changes to the Game class to make it more interractive

--- GameEngines/Game.py
+++ GameEngines/Game.py
@@ -1,5 +1,6 @@
-from typing import Type
+from typing import Type, List
 import time
+import math
 from GameEngines._generic import AbsBoardState, AbsPlayer
 
 
@@ -10,7 +11,12 @@ class Game:
     """
     def __init__(self, board_class: Type[AbsBoardState], p0: AbsPlayer, p1: AbsPlayer):
         self.players = [p0, p1]
-        self.boardState: AbsBoardState = board_class()
+
+        self.board_class = board_class
+        self.history: List[AbsBoardState] = [board_class()]
+        self.time_data: List[float] = []
+
+        self.winner = 0
 
     def play_full(self, results=True):
         """
@@ -19,33 +25,40 @@ class Game:
         :param results: Should the final board be shown in terminal
         :return: the winner, the final score and move data
         """
-        history = []
-        time_data = []
-        if results:
-            print(self.players[0].name, 'vs', self.players[1].name)
+        self.play(math.inf)
 
-        while self.boardState.winner() == 0:
-            turn = self.boardState.turn
+    def play(self, n=1):
+        played = 0
+        while self.winner == 0 and played < n:
+            turn = self.history[-1].turn
             p_nb = turn % 2 + 1
             player = self.players[p_nb - 1]
-            moves = self.boardState.get_legal_moves(p_nb)
+            moves = self.history[-1].get_legal_moves(p_nb)
 
             beg = time.time()
-            res = player.play(self.boardState, moves, p_nb)
-            time_data.append(time.time() - beg)
+            res = player.play(self.history[-1], moves, p_nb)
+            self.time_data.append(time.time() - beg)
 
-            history.append(res)
-            self.boardState = self.boardState.play(res, p_nb)
+            self.history.append(self.history[-1].play(res, p_nb))
+            self.winner = self.history[-1].winner()
+            played += 1
 
-        points = self.boardState.score()
-        winner = self.boardState.winner()
+    def branch(self, i: int) -> 'Game':
+        i += 1
+        new_game = Game(self.board_class, *self.players)
+        new_game.history = self.history[:i]
+        new_game.time_data = self.time_data[:i]
+        new_game.winner = self.winner
 
-        if results:
-            if winner == -1:
-                print(f'Tied: {points}')
-            else:
-                print(f'winner is: player-{winner} {points}')
-            print(f'played turns: {self.boardState.turn}')
-            print(f'Board:\n{self.boardState}')
+        return new_game
 
-        return winner, points, (history, time_data)
+    def show_state(self):
+        points = self.history[-1].score()
+        if self.winner == -1:
+            print(f'Tied: {points}')
+        elif self.winner != 0:
+            print(f'winner is: player-{self.winner} {points}')
+        else:
+            print('Game is ongoing')
+        print(f'played turns: {self.history[-1].turn}')
+        print(f'Board:\n{self.history[-1]}')
