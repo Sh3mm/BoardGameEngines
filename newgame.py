6c1228b00ce877933e88da3955f87c70876dbcbb
sh3mm <antoine.robillard@polymtl.ca>
2024-11-15 19:17:20 -0500

    addition of a script to create new game engine boilerplate quickly

---
+++ newgame.py
@@ -0,0 +1,50 @@
+#! python3
+import argparse
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader, Template
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("GameName", help="name of the new board game")
+parser.add_argument("--templates", "-t", default="templates/", help="the template folder")
+
+
+def main(args: argparse.Namespace):
+    name = args.GameName
+
+    if Path(f"GameEngines/{name}").exists() or Path(f"src/{name}.rs").exists():
+        raise FileExistsError("The Game already exists")
+
+    python_templates = Path(args.templates) / "PythonGame"
+    rust_templates = Path(args.templates) / "RustGame"
+
+    # python template
+    python_engine(name, python_templates)
+
+    # rust template
+    rust_engine(name, rust_templates)
+
+
+def python_engine(name, python_templates):
+    env = Environment(loader=FileSystemLoader(python_templates))
+    for path in env.list_templates():
+        template = env.get_template(path)
+        result = template.render(GameName=name)
+
+        out_file = (Path("GameEngines") / name / path[:-3])
+        out_file.parent.mkdir(exist_ok=True, parents=True)
+        out_file.write_text(result)
+
+
+def rust_engine(name, rust_templates):
+    env = Environment(loader=FileSystemLoader(rust_templates))
+    template = env.get_template("RustGame.rs.j2")
+    result = template.render(GameName=name)
+
+    out_file = (Path("src") / f"{name}.rs")
+    out_file.parent.mkdir(exist_ok=True, parents=True)
+    out_file.write_text(result)
+
+
+if __name__ == '__main__':
+    main(args=parser.parse_args())
