2f802d998fca0d302f6fa7728648d66e74600049
Sh3mm <32112697+Sh3mm@users.noreply.github.com>
2024-12-06 21:44:33 -0500

    made the avalam rust engine slower, but not dependant on a cache of moves (#55)

--- GameEngines/Avalam/SaveModule.py
+++ GameEngines/Avalam/SaveModule.py
@@ -12,8 +12,6 @@ class AvalamSave(AbsSaveModule):
     {
         board: [int],       // Flattened board from (9,9) to (81,)
         ratios: [int]        / Flattened ratio board from (2, 9,9) to (162,)
-        move_cache: [Move], // The move cache from self._moves
-        on_move_call: Move, // The previous move cache from self._on_move_call
         turn: int,          // The current turn
         curr_pid: int     // The active player
     }
@@ -25,12 +23,6 @@ class AvalamSave(AbsSaveModule):
 
         data["board"] = np.array(data["board"]).reshape((9, 9))
         data["ratios"] = np.array(data["ratios"]).reshape((2, 9, 9))
-        data["move_cache"] = set((tuple(i[0]), tuple(i[1])) for i in data["move_cache"])
-
-        if data["on_move_call"] is not None:
-            data["on_move_call"] = (tuple(data["on_move_call"][0]), tuple(data["on_move_call"][1]))
-        else:
-            data["on_move_call"] = None
 
         return AvalamSave._put_data(data, state_type)
 
@@ -45,8 +37,6 @@ class AvalamSave(AbsSaveModule):
         return {
             "board": state.board.flatten().tolist(),
             "ratios": state.ratios.flatten().tolist(),
-            "move_cache": list(state._moves),
-            "on_move_call": state._on_move_call,
             "turn": state.turn,
             "curr_pid": state.curr_pid
         }
@@ -58,8 +48,6 @@ class AvalamSave(AbsSaveModule):
 
         state._board = data["board"]
         state._ratios = data["ratios"]
-        state._moves = data["move_cache"]
-        state._on_move_call = data["on_move_call"]
         state._turn = data["turn"]
         state._curr_pid = data["curr_pid"]
 
--- src/Avalam.rs
+++ src/Avalam.rs
@@ -1,7 +1,5 @@
-use std::cmp::min;
-use std::collections::{HashSet};
-use itertools::{iproduct};
-use ndarray::{Array2, Array3, s, array};
+use itertools::{Itertools};
+use ndarray::{Array2, Array3, array, ArrayView2};
 use numpy::{PyArray2, PyArray3, PyArrayMethods};
 use pyo3::prelude::*;
 use pyo3::{Py, pyclass, pymethods, Python, PyTypeInfo};
@@ -23,10 +21,6 @@ pub struct RawAvalamState {
     _curr_pid: u32,
     #[pyo3(get, set)]
     _turn: u32,
-    #[pyo3(get, set)]
-    _moves: Py<PySet>,
-    #[pyo3(get, set)]
-    _on_move_call: Option<Move>,
 
     #[pyo3(get, set)]
     _save_mod: Py<PyType>
@@ -76,20 +70,43 @@ impl RawAvalamState {
         ]
     }
 
-    /// returns a set of all legal actions that can be used in a specific state
-    fn _get_legal_moves<'py>(&mut self, py: Python<'py>) -> PyResult<Bound<'py, PySet>> {
-        if let Some((origin, dest)) = self._on_move_call {
-            self._on_move_call = None;
-            self._update_move(py, origin, dest)?;
-        }
-        Ok(self._moves.bind(py).to_owned())
-    }
-
     fn default_save_mod() -> Py<PyType> {
         Python::with_gil(|_py| {
             let SaveModule = _py.import("GameEngines.Avalam.SaveModule").unwrap();
             SaveModule.getattr("AvalamSave").unwrap().extract().unwrap()
         })
+    }
+
+    fn _moves_for(abs_board: &Array2<i64>, i: usize, j: usize) -> Vec<Move> {
+        let (_i, _j) = (isize::try_from(i).unwrap(), isize::try_from(j).unwrap());
+        let v = abs_board[(i, j)];
+
+        return [
+            if _i-1 >= 0 && _j-1 >= 0 { Some(((i-1, j-1), abs_board[(i-1, j-1)] + v)) } else { None },
+            if _i-1 >= 0              { Some(((i-1, j  ), abs_board[(i-1, j  )] + v)) } else { None },
+            if _i-1 >= 0 && _j+1 <= 8 { Some(((i-1, j+1), abs_board[(i-1, j+1)] + v)) } else { None },
+            if              _j-1 >= 0 { Some(((i  , j-1), abs_board[(i  , j-1)] + v)) } else { None },
+            if              _j+1 <= 8 { Some(((i  , j+1), abs_board[(i  , j+1)] + v)) } else { None },
+            if _i+1 <= 8 && _j-1 >= 0 { Some(((i+1, j-1), abs_board[(i+1, j-1)] + v)) } else { None },
+            if _i+1 <= 8              { Some(((i+1, j  ), abs_board[(i+1, j  )] + v)) } else { None },
+            if _i+1 <= 8 && _j+1 <= 8 { Some(((i+1, j+1), abs_board[(i+1, j+1)] + v)) } else { None },
+        ].into_iter().filter_map(|option| match option {
+            None => { None }
+            Some((end, pos)) => {
+                if (v < pos) && (pos <= 5) { Some(((i, j), end)) } else { None }
+            }
+        }).collect_vec();
+    }
+
+    fn _has_moves(&self, py: Python) -> bool {
+        let board:ArrayView2<i64> = unsafe { self._board.bind(py).as_array() };
+        let abs_board = board.mapv(|v|v.abs());
+        let towers = abs_board.map(|&v| ((0 < v) & (v < 5)));
+
+        return towers.indexed_iter().find(|((i, j), &v)| {
+            if v { Self::_moves_for(&abs_board, *i, *j).len() > 0 }
+            else { false }
+        }).is_some();
     }
 }
 
@@ -106,14 +123,11 @@ impl RawAvalamState {
 
         let board = PyArray2::from_owned_array(py, RawAvalamState::base_array());
         let ratios = PyArray3::from_owned_array(py, RawAvalamState::base_ratios());
-        let moves = gen_moves(py, &board)?;
 
         return Ok(RawAvalamState {
             _board: board.unbind(),
             _ratios: ratios.unbind(),
             _turn: 0,
-            _moves: moves.unbind(),
-            _on_move_call: None,
             _curr_pid: 1,
             _save_mod: avalam_save
         })
@@ -144,21 +158,17 @@ impl RawAvalamState {
 
     /// copies and returns a python avalam State object
     fn copy<'py>(&self, py: Python<'py>) -> PyResult<Self> {
-        let copy_module = PyModule::import(py, "copy")?;
-        let moves = copy_module.call_method1("copy", (self._moves.bind(py),))?.downcast::<PySet>()?.to_owned();
 
         let board = unsafe { PyArray2::new(py, self._board.bind(py).dims(), false) };
         let ratios = unsafe { PyArray3::new(py, self._ratios.bind(py).dims(), false) };
 
-        self._board.bind(py).copy_to(&board).expect("");
-        self._ratios.bind(py).copy_to(&ratios).expect("");
+        self._board.bind(py).copy_to(&board)?;
+        self._ratios.bind(py).copy_to(&ratios)?;
 
         Ok(RawAvalamState {
             _board: board.unbind(),
             _ratios: ratios.unbind(),
-            _moves: moves.unbind(),
             _turn: self._turn,
-            _on_move_call: None,
             _curr_pid: self._curr_pid,
             _save_mod: self._save_mod.clone_ref(py)
         })
@@ -166,16 +176,10 @@ impl RawAvalamState {
 
     /// play an action on the Avalam State and returns the following State object
     fn play<'py>(&mut self, py: Python<'py>, c_move: Move) -> PyResult<Self> {
-        if let Some(m) = self._on_move_call {
-            self._update_move(py, m.0, m.1)?;
-            self._on_move_call = None;
-        }
-
         let origin = c_move.0;
         let dest = c_move.1;
 
         let mut new_board = self.copy(py)?;
-        new_board._on_move_call = Some((origin, dest));
         new_board._turn += 1;
 
         // board Update
@@ -209,45 +213,28 @@ impl RawAvalamState {
     /// actions the specified player can take. In the case of the Avalam game, both players can
     /// play the same set of moves
     fn get_legal_moves<'py>(&mut self, py: Python<'py>) -> PyResult<Bound<'py, PySet>> {
-        return self._get_legal_moves(py)
-    }
+        let board:ArrayView2<i64> = unsafe { self._board.bind(py).as_array() };
+        let abs_board = board.mapv(|v|v.abs());
+        let towers = abs_board.map(|&v| ((0 < v) & (v < 5)));
 
-    /// updates the current State move cache upon it's creation. Usually by copy.
-    fn _update_move(&self, _py: Python, origin: Coords, dest: Coords) -> PyResult<()> {
-        // Moves Update
-        let move_set = self._moves.bind(_py).downcast::<PySet>()?;
-        let b_ref = self._board.bind(_py);
-        // Impossible origin
-        let i_range = origin.0.saturating_sub(1)..min(origin.0 + 2, 9);
-        let j_range = origin.1.saturating_sub(1)..min(origin.1 + 2, 9);
-        for (i, j) in iproduct!(i_range, j_range) {
-            move_set.discard((origin, (i, j)))?;
-            move_set.discard(((i, j), origin))?;
-        }
-        // Impossible dest
-        let i_range = dest.0.saturating_sub(1)..min(dest.0 + 2, 9);
-        let j_range = dest.1.saturating_sub(1)..min(dest.1 + 2, 9);
-        for (i, j) in iproduct!(i_range, j_range) {
-            move_set.discard((origin, (i, j)))?;
-            move_set.discard(((i, j), origin))?;
-            if unsafe { b_ref.uget(dest).abs() + b_ref.uget((i, j)).abs() } > 5 {
-                move_set.discard(((i, j), dest))?;
-                move_set.discard((dest, (i, j)))?;
-            }
-        }
-        Ok(())
+        let moves = towers.indexed_iter().filter_map(|((i, j), &v)| {
+            if v {
+                Some(Self::_moves_for(&abs_board, i, j))
+
+            } else { None }
+        }).flatten();
+
+        return PySet::new(py, moves);
     }
 
     /// returns the current score of the State. In the case of Avalam, this means the number of
     /// towers controlled by each player
-    fn score(&self) -> (usize, usize){
-        Python::with_gil(|_py| {
-            let array = unsafe { self._board.bind(_py).as_array() };
-            array.fold((0, 0), |b, &v| {
-                if v > 0 { return (b.0 + 1, b.1); }
-                if v < 0 { return (b.0, b.1 + 1); }
-                return b;
-            })
+    fn score<'py>(&self, py: Python<'py>) -> (usize, usize){
+        let array = unsafe { self._board.bind(py).as_array() };
+        array.fold((0, 0), |b, &v| {
+            if v > 0 { return (b.0 + 1, b.1); }
+            if v < 0 { return (b.0, b.1 + 1); }
+            return b;
         })
     }
 
@@ -260,18 +247,11 @@ impl RawAvalamState {
     /// Otherwise, it returns the player id of the winner
     fn winner<'py>(&mut self, py: Python<'py>) -> PyResult<isize> {
         // unfinished
-        if self._get_legal_moves(py)?
-            .call_method0("__len__")?
-            .call_method1("__gt__", (2,))?
-            .is_truthy()? {
-            return Ok(0);
-        }
+        if self._has_moves(py) { return Ok(0); }
 
-        let (p1, p2) = self.score();
+        let (p1, p2) = self.score(py);
         // tie
-        if p1 == p2 {
-            return Ok(-1)
-        }
+        if p1 == p2 { return Ok(-1) }
         // winner
         return Ok(isize::from(p1 < p2) + 1)
     }
@@ -306,32 +286,4 @@ impl RawAvalamState {
 
     #[getter]
     fn ratios(&self) -> &Py<PyArray3<i64>> { return &self._ratios }
-}
-
-
-/// generates all possible action given a specific raw Avalam board configuration
-pub fn gen_moves<'py>(py: Python<'py>, board: &Bound<'py, PyArray2<i64>>) -> PyResult<Bound<'py, PySet>>{
-    let board = unsafe { board.as_array() }.mapv(|v| { v.abs() });
-    let mut moves = HashSet::<Move>::new();
-
-    for (i, j) in iproduct!(0..9, 0..9) {
-        if *board.get([i, j]).unwrap() == 0 { continue; }
-        let sub = board.slice(s![
-                i.saturating_sub(1)..min(i + 2, 9),
-                j.saturating_sub(1)..min(j + 2, 9)
-            ]);
-
-        let legit = sub.indexed_iter()
-            .filter_map(|((_i, _j), &v)| {
-                let _i = _i + (i - (if i != 0 { 1 } else { 0 }));
-                let _j = _j + (j - (if j != 0 { 1 } else { 0 }));
-
-                if (_i, _j).eq(&(i, j)) { return None; }
-                if (v == 0) || (v + *board.get([i, j]).unwrap() > 5) { return None; }
-
-                return Some(((_i, _j), (i, j)));
-            });
-        moves.extend(legit);
-    }
-    return PySet::new(py, moves);
 }
